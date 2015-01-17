9fa17708426481ce4180e98b427eca292bc7cd96
Roland Zsámboki <zsamboki.roland@gmail.com>
2015-01-18 00:29:27 +0100

    Removed a test file

--- dist/packages/test.test.default.js
+++
@@ -1,4 +0,0 @@
-/*! Demand - v1.0.0 - 2015-01-17
-* https://github.com/asec/demand
-* Copyright (c) 2015 Roland Zsámboki; Licensed MIT */
-!function(){alert("testing!"),alert("másik valami :D")}(),demand.demandObject.loaded("test.test.default");
